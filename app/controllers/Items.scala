e162425d1d894887abc20b02696dfb506a9b4e4b
Duesten <t.leendertsen@gmail.com>
2016-09-04 05:37:31 +0200

    Add category browsing LUL

--- app/controllers/Items.scala
+++ app/controllers/Items.scala
@@ -5,18 +5,23 @@ import models.Item
 import play.api.mvc.Controller
 import play.api.mvc.Action
 
+import scalikejdbc.SQLSyntax._
+
 @Singleton
 class Items extends Controller {
 
   def list = Action { implicit req =>
-
     val items: List[Item] = Item.findAll()
-
     Ok(views.html.items.list(items))
   }
 
   def show(id : Long) = Action { implicit req =>
-
     Ok(views.html.items.show(Item.find(id).get))
+  }
+
+  def showCategory(category : String) = Action { implicit req =>
+    val i = Item.syntax("i")
+    val items = Item.findAllBy(where.eq(i.medium,category))
+    Ok(views.html.items.list(items))
   }
 }
