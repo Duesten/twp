76f68a2d17dd93b865dbfc2957f50e67cff80bfc
Duesten <t.leendertsen@gmail.com>
2016-09-04 07:16:52 +0200

    add search function and more shit

--- app/controllers/Items.scala
+++ app/controllers/Items.scala
@@ -1,13 +1,15 @@
 package controllers
 
 import javax.inject._
+import play.api.data._
+import play.api.data.Forms._
 import models.Item
-import play.api.mvc.Controller
-import play.api.mvc.Action
+import _root_.play.api.mvc.{Result, Controller, Action}
 import scalikejdbc._
 import models._
 
 import scalikejdbc.SQLSyntax._
+import views.html.helper.form
 
 @Singleton
 class Items extends Controller {
@@ -26,4 +28,42 @@ class Items extends Controller {
     val items = Item.findAllBy(sqls.eq(i.medium,category))
     Ok(views.html.items.list(items))
   }
+
+  def showCategories = Action { implicit req =>
+    Ok(views.html.items.list(List.empty))
+  }
+
+  def textSearch = Action { implicit req =>
+
+    val itemForm = Form("q" -> text)
+
+
+    val result = itemForm.bindFromRequest().apply("q").value.map{
+      queryString =>  {
+        val i = Item.syntax("i")
+        val likeString = "%"+queryString+"%"
+
+        val dbresult = Item.findAllBy(
+          sqls.like(i.title,likeString).or(
+            like(i.description,likeString)
+          ).or(
+            like(i.year,likeString)
+          ).or(
+            like(i.creators,likeString)
+          ).or(
+            like(i.production,likeString)
+          ).or(
+            like(i.year,likeString)
+          ).or(
+            like(i.genre,likeString)
+          ) or
+            like(i.extra,likeString)
+        )
+        dbresult
+      }
+    }.getOrElse(List.empty)
+
+    Ok(views.html.items.list(result))
+  }
+
 }
